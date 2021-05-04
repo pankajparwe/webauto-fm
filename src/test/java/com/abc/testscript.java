413808d781da2f2246c120d293c4c3d6465dd9fa
Pankaj_parwe <pankaj.parwe@gmail.com>
2021-05-04 14:07:35 +0530

    adding changes

--- src/test/java/com/abc/testscript.java
+++ src/test/java/com/abc/testscript.java
@@ -8,13 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class testscript {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("http://demo.automationtesting.in/Index.html");
-
+        driver.get("http://demo.automationtesting.in/Register.html");
+         /*
         WebElement email = driver.findElement(By.id("email"));
         email.sendKeys("pankaj.parwe@gmail.com");
 
 
         driver.findElement(By.id("enterimg")).click();
+
 
 
         String titleafter = driver.getTitle();
@@ -23,9 +24,15 @@ public class testscript {
         }else{
                 System.out.println("test case is failed");
             }
-        
+       */
 
+        driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("Pankaj");
+        driver.findElement(By.xpath("//input[@ng-model='LastName']")).sendKeys("Parwe");
 
+       driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("amravati");
+        driver.findElement(By.xpath("//input[@ng-model='EmailAdress']")).sendKeys("pankaj.parwe@gmail.com");
+        driver.findElement(By.xpath("//input[@ng-model='Phone']")).sendKeys("9890865105");
+        driver.findElement(By.xpath("//input[@ng-model='radiovalue' and @value ='Male']")).click();
+        driver.findElement(By.xpath("//input[@type='checkbox' and @value ='Cricket']")).click();
     }
-
 }
